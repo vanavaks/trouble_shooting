5414802fbec70063647ee6b1502850ef56e781f8
Иван <vgvanrt@mail.ru>
2016-08-07 15:27:48 +0300

    Validator interfase

--- src/sample/model/Validator.java
+++ src/sample/model/Validator.java
@@ -4,11 +4,19 @@ import java.lang.reflect.Field;
 
 /**
  * Created by Иван on 05.08.2016.
+ * Class for validation of model
  */
 public class Validator {
 
-    public static <M> boolean validate(Field f, Class<M> t){
+   /* public static <M> boolean validate(Field f, Class<M> t){
         return true;
+    }*/
+
+    public static <M> String vadidate(Class<M> t){
+        return null;
     }
 
+    public static <M> String validate(Field f, Class<M> t){
+        return null;
+    }
 }
